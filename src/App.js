4676c961caaf216484973788b5fd247d5ee18680
maurixavi <mxavierarevalo@gmail.com>
2019-12-05 01:14:05 -0300

    App tasks React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation';
+import TasksForm from './components/TasksForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { tasks } from './tasks.json'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks
+    }
+    this.handleAddTodo = this.handleAddTodo.bind(this);
+  }
+
+  handleAddTodo(task) {
+    this.setState({
+      tasks: [...this.state.tasks, task]
+    })
+  }
+
+  render() {
+    const tareas = this.state.tasks.map((task, i) => {
+      return (
+        <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{task.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {task.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              <p>{task.description}</p>
+              <p><mark>{task.responsible}</mark></p>
+            </div>
+          </div>
+        </div>
+      )
+    })
+
+    return (
+      <div className="App">
+        <nav className="navbar navbar-dark bg-dark">
+            <a href="" className="text-white">
+                Tasks APP
+            </a>
+            <span className="badge badge-pill badge-light ml-2">
+            { this.state.tasks.length } 
+        </span>
+        </nav>
+            
+        
+
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col-md-4">
+              <TasksForm  onAddTodo={this.handleAddTodo} />
+            </div>
+          </div>
+
+          <div className="col-md-9">
+            <div className="row">
+              { tareas }
+            </div>
+          </div>
+        </div>
+        
+      
+      </div>
+    );
+  }
 }
 
 export default App;
